9ec8d2114f0e05d0f9ef018d27482ed4975ed3a1
Matthew Ragan <raganmd@gmail.com>
2019-08-27 20:21:14 -0700

    UI update
    Updates UI with better sizing
    Updates to extension for setting UI

--- old-touchdesigner-examples/dos-and-donts/modules/container_ui/uiEXT.py
+++ old-touchdesigner-examples/dos-and-donts/modules/container_ui/uiEXT.py
@@ -7,3 +7,18 @@ class DosDontsUI:
         html_path = self.Myop.op('null_ref_data')[selectedRow + 1, 'html_file']
         self.Myop.op('container_web').par.Url = html_path
         pass
+    
+    def Set_panes(self, selectedRow):
+    
+        selected_module     = self.Myop.op('null_ref_data')[selectedRow + 1, 'path']
+        do_pane             = '{}/base_do'.format(selected_module)
+        dont_pane           = '{}/base_dont'.format(selected_module)
+
+        td_do_pane          = ui.panes[1]
+        td_dont_pane        = ui.panes[2]
+        td_do_pane.owner    = op(do_pane)
+        td_do_pane.home()
+        
+        td_dont_pane.owner  = op(dont_pane)
+        td_dont_pane.home()
+        pass
